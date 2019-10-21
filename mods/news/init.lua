4c7b5762f6a0f7dbc8a345378117e4fc8e77a29c
Cahrs <55798443+Cahrs@users.noreply.github.com>
2019-10-21 19:57:23 -0400

    update news mod

--- mods/news/init.lua
+++ mods/news/init.lua
@@ -1,46 +1,84 @@
-local chat_reasons = {}
-chat_reasons.set_hp = {
+news = {}
+
+news.chat_reasons = {}
+news.chat_reasons.nodes = {}
+news.chat_reasons.groups = {}
+news.chat_reasons.set_hp = {
     "mysteriously vanishes"
 }
-chat_reasons.punch = {
+news.chat_reasons.punch = {
     "was sliced' and diced' by",
     "got pwned by",
     "got a-salt-ed by",
 }
-chat_reasons.node_damage = {
-    "played with fire",
-    "took a bath in lava",
-    "ate a chili-pepper",
-}
-chat_reasons.fall = {
+news.chat_reasons.fall = {
     "hit the ground to hard",
     "fell into the abyss",
     "walked off a cliff",
 }
-chat_reasons.drown = {
+news.chat_reasons.drown = {
     "fell asleep in the tub",
     "blew one to many bubbles",
     "tried to drink the ocean",
 }
 
+function news.register_deathmsg_tbl(type, name, msgs)
+    if not (type or name or msgs) then
+        return
+    end
+
+    if news.chat_reasons[type] then
+        news.chat_reasons[type][name] = msgs
+    end
+end
+
 minetest.register_on_dieplayer(function(obj, reason)
-    if reason.type and chat_reasons[reason.type] then
+    if reason.type and news.chat_reasons[reason.type] then
         local killer = ""
-        local news = ""
-        if reason.object then
-            killer = reason.object:get_player_name()
-            news = " BREAKING NEWS: Local man \"" .. obj:get_player_name() .. "\" " .. chat_reasons[reason.type][math.random(1, #chat_reasons[reason.type])] .. "\"" .. killer .. "\"."
-        else
-            news = " BREAKING NEWS: Local man \"" .. obj:get_player_name() .. "\" " .. chat_reasons[reason.type][math.random(1, #chat_reasons[reason.type])] .. "."
+        local news_msg = ""
+        local reason_msg = ""
+        local player = obj:get_player_name()
+        local node = minetest.registered_nodes[minetest.get_node(obj:get_pos()).name]
+        local num = nil
+        print(node.name)
+
+        if reason.type == "node_damage" then
+            --print("is node_damage")
+            if node and news.chat_reasons.nodes[node.name] and #news.chat_reasons.nodes[node.name] > 0 then
+                num = math.random(1, #news.chat_reasons.nodes[node.name])
+                reason_msg = news.chat_reasons.nodes[node.name][num]
+            elseif node and node.groups then
+                for _, groupname in pairs(node.groups) do
+                    if news.chat_reasons.groups[groupname] and #news.chat_reasons.groups[groupname] > 0 then
+                        num = math.random(1, #news.chat_reasons.groups[groupname])
+                        reason_msg = news.chat_reasons.groups[groupname][num]
+                        break
+                    end
+                end
+            end
+        elseif news.chat_reasons[reason.type] and #news.chat_reasons[reason.type] > 0 then
+            num = math.random(1, #news.chat_reasons[reason.type])
+            reason_msg = news.chat_reasons[reason.type][num]
         end
-        print(news)
+
+        if reason.object then
+            if reason.object:is_player() then
+                killer = reason.object:get_player_name()
+            else
+                killer = reason.object:get_luaentity().name
+            end
+
+            news_msg = " BREAKING NEWS: Local man \"" .. player .. "\" " .. reason_msg .. " \"" .. killer .. "\"."
+        else
+            news_msg = " BREAKING NEWS: Local man \"" .. player .. "\" " .. reason_msg .. "."
+        end
 
         local station = "[BBC News]"
         station = minetest.colorize("#a8659c", station)
 
-        news = minetest.colorize("#7f99b1", news)
+        news_msg = minetest.colorize("#7f99b1", news_msg)
 
-        minetest.chat_send_all(station .. news)
+        minetest.chat_send_all(station .. news_msg)
     end
 end)
 
@@ -50,30 +88,36 @@ minetest.register_chatcommand("news", {
         ban = true
     },
     func = function(station, param)
-        local station, news = string.match(param, "^([%d%a_-]+) ([%d%a%s%p%%_-]+)$")
+        local station, news_msg = string.match(param, "^([%d%a_-]+) ([%d%a%s%p%%_-]+)$")
         local bool = string.sub(param, -1, -1)
 
-        if station and news then
+        if station and news_msg then
             --print(station, news, bool)
                 
             station = minetest.colorize("#a8659c", "[" .. string.upper(station) .. " News" .. "]")
             
             if bool and bool == "$" then
-                news = string.sub(news, 1, #news-1)
-                news = "BREAKING NEWS: " .. news
+                news_msg = "BREAKING NEWS: " .. string.sub(news, 1, #news_msg - 1)
             end
         
-            news = minetest.colorize("#7f99b1", news)
+            news_msg = minetest.colorize("#7f99b1", news_msg)
 
-            minetest.chat_send_all(station .. " " .. news)
+            minetest.chat_send_all(station .. " " .. news_msg)
         else
             local station = "[BBC News] "
             station = minetest.colorize("#a8659c", station)
 
-            local news = "BREAKING NEWS: salt levels rise on PK."
-            news = minetest.colorize("#7f99b1", news)
+            local news_msg = "BREAKING NEWS: Salt levels rising."
+            news_msg = minetest.colorize("#7f99b1", news)
 
-            minetest.chat_send_all(station .. news)
+            minetest.chat_send_all(station .. news_msg)
         end
     end
+})
+
+news.register_deathmsg_tbl("nodes", "default:lava_source", 
+{
+    "melted into a ball of fire",
+    "couldn't resist the warm glow of lava",
+    "dug straight down"
 })
